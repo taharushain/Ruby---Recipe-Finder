7c11ac199c25b10905e5dc7f6aecf2f465076d07
Taha Rushain <taharushain@gmail.com>
2016-01-07 03:27:46 +0500

    Updated

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,16 @@
 class Recipe
  include HTTParty
- ENV["FOOD2FORK_KEY"] = 'your API key'
- base_uri 'http://food2fork.com/api'
+
+ ENV["FOOD2FORK_KEY"] = 'YOUR_API_KEY'
+
+ key_value = ENV["FOOD2FORK_KEY"]
+
+ hostport = ENV["FOOD2FORK_SERVER_AND_PORT"] || "www.food2fork.com"
+
+ base_uri "http://#{hostport}/api"
+
+ 
+ #base_uri 'http://food2fork.com/api'
  default_params key: ENV["FOOD2FORK_KEY"]
  format :json
 
